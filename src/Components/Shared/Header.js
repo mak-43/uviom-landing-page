bdea609c7e5b4f63c442fd305e15753077e63d23
Mak-43 <letsmailmak@gmail.com>
2022-06-13 01:11:02 +0600

    header without responsive

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from '../Assets/logo.png'
 const Header = () => {
     return (
-        <div className='bg-[#172d41] py-3'>
-            <div className='flex items-center'>
+        <div className='bg-[#172d41] py-3 '>
+            <div className='flex items-center  justify-between'>
                 <div className='flex  items-start'>
-                    <div className='divide-x-2'>
+                    <div className=''>
                         <img src={logo} alt="" />
                     </div>
                     <div className='flex flex-col justify-center px-2 '>
@@ -13,16 +13,19 @@ const Header = () => {
                         <span className='text-xs text-orange-300'>Your Mission Is Our Vision</span>
                     </div>
                 </div>
-                <div className='flex gap-5 text-white'>
+                <div className='flex gap-5 text-white px-5'>
                     <a href="#">Home</a>
                     <a href="#">About</a>
                     <a href="#">Career</a>
                     <a href="#">Privacy</a>
                     <a href="#">Follow Us</a>
+                    <p><i class="fa-solid fa-gear"></i></p>
+                    <p><i class="fa-brands fa-facebook"></i></p>
+                    <p><i class="fa-brands fa-instagram"></i></p>
                 </div>
-                <div>
-
-                </div>
+                {/* <div className='flex  text-white gap-4'>
+                    
+                </div> */}
             </div>
 
 
