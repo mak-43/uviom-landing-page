0bafa499a92a3ccb0fc8e1d0d464b8290bcfa9dd
Mak-43 <letsmailmak@gmail.com>
2022-06-13 21:56:09 +0600

    responsive

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,30 +1,35 @@
 import React, { useState } from 'react';
 import logo from '../Assets/logo.png'
 import './Header.css'
-
+import Typewriter from 'typewriter-effect'
 const Header = () => {
-    const [isOpen,setIsOpen]=useState(false)
-    
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className='bg-[#172d41] px-10 '>
-            
+
             <div className='flex flex-wrap  Navbar'>
-                <div className='flex   items-start  flex-wrap nav-log'>
-                    <div className=' flex justify-center items-center my-3'>
+                <div className='flex   items-start  flex-wrap nav-log '>
+                    <div className=' flex justify-center items-center my-3 '>
                         <img src={logo} alt="" />
                     </div>
 
                     <div className='flex   '>
                         <hr className='my-auto text-white' style={{ transform: 'rotate(90deg)', width: '50px' }} />
                         <div className=' flex flex-col gap-1 my-3 justify-center items-center'>
-                            <h1 className='text-4xl font-bold text-slate-500 '>UVIOM</h1>
-                            <span className='mission '>Your Mission Is Our Vision</span>
+                            <h1 className='text-4xl font-bold text-slate-500 mx-2'>UVIOM</h1>
+                            <span className='mission '> <Typewriter options={{ sutoStart: true, loop: true }}
+                                onInit={(typewriter) => {
+                                    typewriter.typeString('Your Mission Is Our Vission').pauseFor(2000).deleteAll().start()
+
+                                }}
+                            /></span>
                         </div>
                     </div>
-                   
+
                 </div>
-                <div id='navigation' className={`flex gap-6 text-white items-center lg:flex-row md:flex-row sm:flex-col nav-items ${isOpen && 'open'} `}>
-                
+                <div id='navigation' className={`flex gap-6 text-white items-center nav-items ${isOpen && 'open'} `}>
+
                     <a className='hover:text-slate-500 ' href="#">Home</a>
                     <a className='hover:text-slate-500' href="#">About</a>
                     <a className='hover:text-slate-500' href="#">Career</a>
@@ -36,13 +41,13 @@ const Header = () => {
                         <p className='hover:text-slate-500'><i class="fa-brands fa-instagram"></i></p>
                     </div>
                 </div>
-                <div onClick={()=>setIsOpen(!isOpen)} 
-                className={`nav-toggle ${isOpen && 'open'}`}
+                <div onClick={() => setIsOpen(!isOpen)}
+                    className={`nav-toggle ${isOpen && 'open'}`}
                 >
                     <div className='bar'></div>
                 </div>
             </div>
-            
+
 
         </div>
     );
